a21a2ef26dabb3af4ed6f2ac3398ad5d09a81c97
Changgil Jeong <ombreman21@gmail.com>
2021-07-06 00:26:02 +0100

    adit comments

--- models/user.js
+++ models/user.js
@@ -5,10 +5,13 @@ const UserSchema = new mongoose.Schema({
   nickname: String,
   password: String,
 });
+
 UserSchema.virtual("userId").get(function () {
   return this._id.toHexString();
 });
+
 UserSchema.set("toJSON", {
   virtuals: true,
 });
+
 module.exports = mongoose.model("User", UserSchema);
